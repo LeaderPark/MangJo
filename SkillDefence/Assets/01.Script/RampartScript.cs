e46b7c9817ef9a92370c93e42fa18cc6f353ee1d
KimTaeHyen <62549012+teahyen@users.noreply.github.com>
2021-10-14 14:10:51 +0900

    두트윈+게임메니저
    기본 베이스 넣기

---
+++ SkillDefence/Assets/01.Script/RampartScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RampartScript : MonoBehaviour
+{
+    public Scrollbar Rampar_Hp_Bar;
+    public Text Rampar_Hp_Text;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
