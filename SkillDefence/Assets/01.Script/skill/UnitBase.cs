8f191392b06ac0c5678824c86239397c0c5492be
LeaderPark <dkdleldjqkr976@gmail.com>
2021-11-05 15:17:51 +0900

    plus
    plus

--- SkillDefence/Assets/01.Script/skill/UnitBase.cs
+++ SkillDefence/Assets/01.Script/skill/UnitBase.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class UnitBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    protected float unitHP;
 
-    // Update is called once per frame
-    void Update()
+    public virtual void unitSkills()
     {
-        
+
     }
+    
 }
